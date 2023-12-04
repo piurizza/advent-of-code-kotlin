7ad3057a2ad6efe92bc4666c7ae18f16daea2f8f
Pietro Iurizza <piurizza@cuebiq.com>
2023-12-04 13:09:14 +0100

    feat: d04 part 1 and part 2 completed

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ Many thanks to Jadarma and its [Advent of Code Kotlin Template][template] for ma
 - [1 - Trebuchet?!](https://github.com/piurizza/advent-of-code-kotlin/blob/main/solutions/aockt/y2023/Y2023D01.kt)
 - [2 - Cube Conundrum](https://github.com/piurizza/advent-of-code-kotlin/blob/main/solutions/aockt/y2023/Y2023D02.kt)
 - [3 - Gear Ratios](https://github.com/piurizza/advent-of-code-kotlin/blob/main/solutions/aockt/y2023/Y2023D03.kt)
+- [4 - Scratchcards](https://github.com/piurizza/advent-of-code-kotlin/blob/main/solutions/aockt/y2023/Y2023D04.kt)
 
 [aoc]: https://adventofcode.com
 [github]: https://github.com/piurizza
---
+++ solutions/aockt/y2023/Y2023D04.kt
@@ -0,0 +1,111 @@
+package aockt.y2023
+
+import io.github.jadarma.aockt.core.Solution
+
+object Y2023D04 : Solution {
+
+    private fun parseInput(input: String): List<String> =
+        input
+            .splitToSequence('\n')
+            .toList()
+
+    override fun partOne(input: String): Any {
+        return parseInput(input)
+            .map { cardString ->
+                val currentCard = parseCard(cardString)
+
+                val winnersCount = currentCard.winningNumbers
+                    .intersect(currentCard.numbersToCheck)
+                    .count()
+
+                if (winnersCount == 0) return@map 0
+
+                1 shl (winnersCount - 1)
+
+            }
+            .reduce { acc, points -> acc + points }
+    }
+
+    override fun partTwo(input: String): Int {
+        val cardsWinnersCount = parseInput(input)
+            .map { cardString ->
+                val currentCard = parseCard(cardString)
+
+                val winnersCount = currentCard.winningNumbers
+                    .intersect(currentCard.numbersToCheck)
+                    .count()
+
+                CardsWinnerCard(
+                    currentCard.cardId,
+                    winnersCount,
+                    count = 1
+                )
+            }
+            .toMutableList()
+
+        generateWonCards(cardsWinnersCount)
+
+        return cardsWinnersCount
+            .map { it.count }
+            .reduce { acc, cardNumber -> acc + cardNumber }
+    }
+
+    private fun parseCard(cardString: String): PointsWinnerCard {
+        val matches = Regex("\\s(\\d{0,2}\\s{0,2})*")
+            .findAll(cardString)
+            .toList()
+
+        val cardId = matches[0].value
+            .trimStart()
+            .toInt()
+
+        val winningNumbers = matches[1].value
+            .trim()
+            .split(" ")
+            .mapNotNull { numberString ->
+                if (numberString.isNotEmpty())
+                    numberString.toInt()
+                else null
+            }
+            .toSet()
+
+        val numbersToCheck = matches[2].value
+            .trimEnd()
+            .split(" ")
+            .mapNotNull { numberString ->
+                if (numberString.isNotEmpty())
+                    numberString.toInt()
+                else null
+            }
+            .toSet()
+
+        return PointsWinnerCard(cardId, winningNumbers, numbersToCheck)
+    }
+
+    private fun generateWonCards(cardsWinnersCount: MutableList<CardsWinnerCard>) =
+        cardsWinnersCount
+            .map { card ->
+                if (card.winnersCount == 0) return@map
+
+                (card.cardId until card.cardId + card.winnersCount)
+                    .map { index ->
+                        cardsWinnersCount[index] = cardsWinnersCount[index]
+                            .copy(
+                                count = cardsWinnersCount[index].count
+                                    + card.count
+                            )
+                    }
+            }
+
+    data class PointsWinnerCard(
+        val cardId: Int,
+        val winningNumbers: Set<Int>,
+        val numbersToCheck: Set<Int>
+    )
+
+    data class CardsWinnerCard(
+        val cardId: Int,
+        val winnersCount: Int,
+        var count: Int
+    )
+}
